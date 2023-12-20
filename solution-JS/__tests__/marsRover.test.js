dc356bbc6235c5cd779dd328a961ba13dd1a2165
scorcuera <scorcuera6@gmail.com>
2023-12-20 10:11:35 +0100

    Translate tests dedscriptions to English

--- solution-JS/__tests__/marsRover.test.js
+++ solution-JS/__tests__/marsRover.test.js
@@ -1,61 +1,54 @@
-import { moveRover } from '../src/marsRover'
+import { moveRover } from "../src/moveRover";
 
 describe('Mars Rover', () => {
 
-    it('El Rover se mantiene en la posición original si no le damos órdenes', () => {
+    test('should stay put if no orders are given', () => {
 
         let position = moveRover([]);
-
         expect(position).toEqual([100, 100]);
 
     })
 
-    it('El Rover se mueve al norte', () => {
+   test('should move towards North', () => {
 
         let position = moveRover(['N']);
-        console.log(position);
-
         expect(position).toEqual([101, 100]);
 
     })
 
-    it('El Rover se mueve al sur', () => {
+   test('should move towards South', () => {
 
         let position = moveRover(['S']);
-
         expect(position).toEqual([99, 100]);
 
     })
 
-    it('El Rover se mueve al este', () => {
+   test('should move towards East', () => {
 
         let position = moveRover(['E']);
-
         expect(position).toEqual([100, 101]);
 
     })
 
-    it('El Rover se mueve al oeste', () => {
+   test('should move towards West', () => {
 
         let position = moveRover(['W']);
-
         expect(position).toEqual([100, 99]);
 
     })
 
 
-    it('El Rover realiza múltiples movimientos en la misma dirección', () => {
+   test('should makes several movements towards the same direction', () => {
 
         let position = moveRover(['E', 'E']);
-
         expect(position).toEqual([100, 102]);
 
     })
 
-    it('El Rover realiza múltiples movimientos en diferentes direcciones', () => {
+   test('should makes several movements towards different directions', () => {
 
-        let position = moveRover(['E', 'N', 'N', 'E', 'E', 'S', 'S', 'S', 'S', 'W']);
-
+        let position = moveRover(['E', 'N', 'N', 'E', 'E', 'S', 'S', 'S', 'S', 'W'])
         expect(position).toEqual([98, 102]);
     })
+
 });
